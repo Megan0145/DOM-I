196328a55791ecf52c26e52a8aed8b3fcab756d8
Megan <mennis0145@gmail.com>
2019-09-03 09:03:23 +0100

    For loop implemented for navlink content

--- js/index.js
+++ js/index.js
@@ -46,12 +46,16 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 let navLinks = document.querySelectorAll('nav a');
 navLinks = Array.from(navLinks);
 
-navLinks[0].textContent = siteContent['nav']['nav-item-1'];
-navLinks[1].textContent = siteContent['nav']['nav-item-2'];
-navLinks[2].textContent = siteContent['nav']['nav-item-3'];
-navLinks[3].textContent = siteContent['nav']['nav-item-4'];
-navLinks[4].textContent = siteContent['nav']['nav-item-5'];
-navLinks[5].textContent = siteContent['nav']['nav-item-6'];
+for (let i =0; i< navLinks.length; i++) {
+  navLinks[i].textContent = siteContent['nav']['nav-item-' + (i + 1)];
+}
+
+// navLinks[0].textContent = siteContent['nav']['nav-item-1'];
+// navLinks[1].textContent = siteContent['nav']['nav-item-2'];
+// navLinks[2].textContent = siteContent['nav']['nav-item-3'];
+// navLinks[3].textContent = siteContent['nav']['nav-item-4'];
+// navLinks[4].textContent = siteContent['nav']['nav-item-5'];
+// navLinks[5].textContent = siteContent['nav']['nav-item-6'];
 
 
 //cta
